7f7cf0957d74e3936b4c172724be520d23805fdf
Andres Chamorro <andres.chamorro@upr.edu>
2022-08-23 17:12:41 -0400

    more complete bpe tokenizer

--- scripts/tokenizer_transcripts.py
+++ scripts/tokenizer_transcripts.py
@@ -1,77 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
-from transformers import GPT2Config, TFGPT2Model
 from tokenizers import ByteLevelBPETokenizer
 
-# Initialize a GPT2 Model
-gpt2_config = GPT2Config()
-model = TFGPT2Model(gpt2_config)
 # Initialize a tokenizer
 tokenizer = ByteLevelBPETokenizer()
-
-
-# In[2]:
-
 
 # importing module
 import sys
 # appending a path
 sys.path.extend(['.', '..'])
-
-
-# In[3]:
-
-
 import datasets
 
 trans_data = datasets.load_dataset('../loaders/dataset_script.py', data_dir='data')
 trans_data = trans_data.shuffle(seed=42)
 trans_data = trans_data.map(lambda seq: {"feature": seq["feature"].upper()}, num_proc=4)
 
-
-# In[4]:
-
-
-import random
-
-k_low = 12
-k_high = 12
+k = 17
 
 def kmernizer(seq):
-    return " ".join([seq[i: i + random.randint(k_low, k_high + 1)] for i in range(len(seq) - k_high + 1)])
+    return " ".join([seq[i: i + k] for i in range(len(seq) - k + 1)])
 
 def batch_kmer(batch):
     return [kmernizer(seq) for seq in batch]
 
-
-# In[7]:
-
-
 trans_data = trans_data.map(lambda seq: {"feature": kmernizer(seq["feature"])}, num_proc=4)
 
-
-# In[8]:
-
-
-batch_size = 1000
+batch_size = 1024
 
 def get_training_corpus():
     for i in range(0, len(trans_data["train"]), batch_size):
         yield trans_data["train"][i : i + batch_size]["feature"]
 
-
-# In[ ]:
-
-
-tokenizer.train_from_iterator(get_training_corpus(), vocab_size=6781480)
-
-
-# In[ ]:
-
-
-tokenizer.save_pretrained("transcripts_gpt2_tokenizer")
+tokenizer.train_from_iterator(
+        get_training_corpus(),
+        vocab_size=100000, 
+        show_progress=True,
+        min_frequency=2, 
+        special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>",])
+tokenizer.save_pretrained("transcripts_bpe_tokenizer")
 
