1d801f16d4619f127eed05926a3f073622e21ba1
Andres Chamorro <andres.chamorro@upr.edu>
2022-09-30 09:48:35 -0400

    mlm tokenized random take fix

--- scripts/run_mlm.py
+++ scripts/run_mlm.py
@@ -440,14 +440,14 @@ def main():
                 tokenizer = TOKENIZER_TYPES[model_args.tokenizer_name](
                         model_args.tokenizer_vocab, 
                         model_args.tokenizer_merges)
-                tokenizer = PreTrainedTokenizerFast(
-                        tokenizer_object=tokenizer, 
-                        unk_token="[UNK]",
-                        sep_token="[SEP]",
-                        cls_token="[CLS]",
-                        pad_token="[PAD]",
-                        mask_token="[MASK]",
-                        model_max_length=model_args.tokenizer_max_length, truncation_side='right')
+             tokenizer = PreTrainedTokenizerFast(
+                     tokenizer_object=tokenizer, 
+                     unk_token="[UNK]",
+                     sep_token="[SEP]",
+                     cls_token="[CLS]",
+                     pad_token="[PAD]",
+                     mask_token="[MASK]",
+                     model_max_length=model_args.tokenizer_max_length, truncation_side='right')
         except KeyError:
             tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name)
         else:
@@ -531,7 +531,7 @@ def main():
         )
 
         # Log a few random samples from the training set:
-        for index in random.sample(range(len(train_dataset)), min(3, len(tokenized_datasets))):
+        for index in random.sample(range(len(tokenized_datasets)), min(3, len(tokenized_datasets))):
             logger.info(f"Sample {index} of the training set: {raw_datasets[index]}.")
             logger.info(f"Sample {index} of the training set: {tokenized_datasets[index]}.")
         # Main data processing function that will concatenate all texts from our dataset and generate chunks of
